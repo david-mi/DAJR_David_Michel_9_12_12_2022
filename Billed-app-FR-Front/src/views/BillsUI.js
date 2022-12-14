9ac954e10b84088b77739d52ce9a44bd129580a8
David Michel <david.michel2809@gmail.com>
2022-12-14 10:33:40 +0100

    feat: add sort method in row function from BillsUI.js, to sort bills in ascending order #7

--- Billed-app-FR-Front/src/views/BillsUI.js
+++ Billed-app-FR-Front/src/views/BillsUI.js
@@ -20,11 +20,19 @@ const row = (bill) => {
   }
 
 const rows = (data) => {
-  return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
+  return (data && data.length) 
+  ? [...data]
+    .sort((a, b) => {
+      return new Date(a.date) < new Date(b.date) 
+        ? 1 
+        : -1
+    })
+    .map(bill => row(bill))
+    .join("")
+  : ""
 }
 
 export default ({ data: bills, loading, error }) => {
-  
   const modal = () => (`
     <div class="modal fade" id="modaleFile" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
